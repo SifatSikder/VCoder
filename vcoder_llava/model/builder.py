76d81c8466dfb758e58d570a83ffc979ba42ad58
SifatSikder <bsse1221@iit.du.ac.bd>
2024-07-13 00:45:14 +0600

    added: load_in_8bit_fp32_cpu_offload in builder.py

--- vcoder_llava/model/builder.py
+++ vcoder_llava/model/builder.py
@@ -29,6 +29,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
 
     if load_8bit:
         kwargs['load_in_8bit'] = True
+        kwargs['load_in_8bit_fp32_cpu_offload'] = True
     elif load_4bit:
         kwargs['load_in_4bit'] = True
         kwargs['quantization_config'] = BitsAndBytesConfig(
